675ebb1617854489c94b16e1f08b83f6f395d3e0
nima <nimaqanbari1999@gmail.com>
2022-02-28 00:29:59 -0800

    add useReducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from 'react'
-import "./App.scss"
+import React, { useState, useReducer } from "react";
+import "./App.scss";
+
+const initialState = {
+  lastNoteCreated: null,
+  totalNotes: 0,
+  notes: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_NOTE": {
+      const newState = {
+        lastNoteCreated: new Date.toTimeString().slice(0, 8),
+        totalNotes: state.notes.length + 1,
+        notes: [...state.notes, action.payload],
+      };
+      return newState;
+    }
+
+    default:
+      return state;
+  }
+};
 
 const App = () => {
+  const [inputNotes, setInputNotes] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const addNote = () => {}
+  const addNote = () => {};
 
   return (
-    <div className='app'>
-      <h1>
-        Sticky Notes
-      </h1>
+    <div className="app">
+      <h1>Sticky Notes</h1>
       <form onSubmit={addNote} className="note-form">
-        <textarea placeholder='Create a new note...'></textarea>
+        <textarea
+          placeholder="Create a new note..."
+          value={inputNotes}
+          onChange={(e) => setInputNotes(e.target.value)}
+        ></textarea>
         <button>Add</button>
+
+        {inputNotes}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
